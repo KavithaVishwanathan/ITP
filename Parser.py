7165a4ebf17a7041289123757db8a1c5883192ee
kavi1612 <kavi1612@users.noreply.github.com>
2015-11-26 18:22:32 -0500

    Updated to get the text content for the database

--- Parser.py
+++ Parser.py
@@ -29,3 +29,35 @@ def FindPageName(text):
         if "<title>" in line:
             pgName = GettingBetweenTags(line)
             print pgName
+
+
+
+##To get text type contents from a html file
+
+#to get the required sectios to pull text content            
+def SplitTextByTags(filePath, search_begin_str, search_end_str):
+    
+    txtfile = open(filePath,"r")
+    contentlist = []
+    content = ""
+    flag = 0
+    for line in txtfile:
+        if(search_begin_str in line) or (flag == 1):
+            content = content + line.strip()
+            if not(search_end_str in line):
+                flag = 1
+            else:
+                flag = 0
+                contentlist.append(content.strip())
+                content = ""
+    return contentlist
+
+#to post to the TextContent table
+def GetTextContent():
+    contentlist = SplitTextByTags("Data/brg001-200.txt", """color="#FF0000">Car ""","</p>")
+    for line in contentlist:
+        pgline = line
+        while (">" in pgline):
+            pgline = GettingBetweenTags(pgline,">","</")
+        contentline = RemoveTags(line)
+        print pgline + " " + contentline
